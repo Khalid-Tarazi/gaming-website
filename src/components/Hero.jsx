a9447adaa9d5b0063fef5433229f1866af318d17
UNDAMAG3D <k.tarazi@yahoo.com>
2025-10-03 03:27:30 +0300

    hero section, video animation.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,8 @@
-import React, { use } from 'react'
-import { useState } from 'react'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { TiLocationArrow } from "react-icons/ti";
-
 import Button from "./Button";
+import { gsap } from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 
 const Hero = () => {
@@ -27,6 +26,30 @@ const Hero = () => {
 
     setCurrentIndex(upcomingVideoIndex);
   }
+
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.set('#next-video', { visibility: 'visible' });
+
+        gsap.to('#next-video', { 
+          transformOrigin: 'center center',
+          scale: 1,
+          width: '100%',
+          height: '100%',
+          duration: 1,
+          ease: 'power2.inOut',
+          onStart: () => nextVidoRef.current.play(),
+        })
+
+        gsap.from ('#current-video', { 
+          transformOrigin: 'center center',
+          scale: 0,
+          duration: 1.5,
+          ease: 'power2.inOut',
+        })
+      }
+    }, { dependencies: [currentIndex], revertOnUpdate: true });
 
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
@@ -60,7 +83,7 @@ const Hero = () => {
 
           <video
             src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
-            // autoPlay
+            autoPlay
             loop
             muted
             className="absolute left-0 top-0 size-full object-cover object-center"
